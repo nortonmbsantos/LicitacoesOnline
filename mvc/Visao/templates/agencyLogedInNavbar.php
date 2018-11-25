c18b802fb6db1230054a357950bead1126ec273b
Norton M. B. Santos <norton@alunos.utfpr.edu.br>
2018-11-25 14:25:56 -0200

    Layout fixes

--- mvc/Visao/templates/agencyLogedInNavbar.php
+++ mvc/Visao/templates/agencyLogedInNavbar.php
@@ -1,18 +1,14 @@
-<li class="nav-item">
-        <a class="nav-link" href="<?= URL_RAIZ . 'agency/biddings' ?>">Minhas Licitações</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-      </ul>
+  <li class="nav-item">
+    <a class="nav-link" href="<?= URL_RAIZ . 'agency/biddings' ?>">Minhas Licitações</a>
+  </li>
+</ul>
 
 <div class="nav-item dropdown mr-10">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     <?php echo $this->getAgency()->getAgencyName(); ?><i class="fas fa-archway ml-1"></i>
     </a>
     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-    <a class="dropdown-item" href="<?= URL_RAIZ . 'user' ?>">Perfil</a>
-    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="<?= URL_RAIZ . 'agency/biddings' ?>">Minhas Licitações</a>
     <div class="dropdown-divider"></div>
     <a class="dropdown-item" href="<?= URL_RAIZ . 'agency/logout' ?>">Logout</a>
     </div>
