72399c02911417541180325d1191607a1ed980b2
Norton M. B. Santos <norton@alunos.utfpr.edu.br>
2018-12-07 12:52:17 -0200

    removing extra divider

--- mvc/Visao/bidding/closedBidding.php
+++ mvc/Visao/bidding/closedBidding.php
@@ -5,7 +5,6 @@
     <p> Empresa vencedora: <?php echo $bidding->getWinner()->getUsername() ?></p>
     <p> Valor: R$<?php echo $bidding->getValue() ?></p>
 
-<div class="dropdown-divider"></div>
     <?php require_once('bidList.php') ?>
 </div>
 <?php } else { ?>
